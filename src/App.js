7b07f1ccaba3672349daddb9326cbf837a9b00eb
bobnearents <bobnearents@yahoo.com>
2019-02-04 11:17:21 -0800

    finzalized functionality

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-expressions */
 import React from 'react';
 
 function Card(props) {
@@ -11,28 +12,34 @@ function Card(props) {
 
 function List(props) {
   // const cards = [<Card />, <Card />];
-
+  const cards = props.cards.map((card) => { return <Card key = {card.id} title={card.card.title} content={card.card.content} /> });
   return (
     <section className="List">
       <header className="List-header">
         <h2>{props.header}</h2>
       </header>
       <div className="List-cards">
-        {props.cards}
+        {cards}
       </div>
     </section>
   );
 }
 
+
+
 function App(props) {
   
+  const grabCards = function(cardIds) {
+    return cardIds.map(id => { return { id, card: props.store.allCards[id] } })
+  };
+
   const lists = props.store.lists.map(list => {
-    <List key={list.id} cards={list.cardIds} header={list.headers} />
-  })
+    return <List key={list.id} cards={grabCards(list.cardIds)} header={list.header} />
+  });
 
   return (
     <main className ="App">
-      <header class="App-header">
+      <header className="App-header">
         <h1>Trelloyes!</h1>
       </header>
       <div className="App-list">

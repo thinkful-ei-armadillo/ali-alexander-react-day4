5f525559ab67add6e06c6cc510e1ab3b5653646a
AReach <DerAReich@gmail.com>
2019-02-04 11:28:54 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,35 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+function Card(props) {
+  return (
+    <div className="Card">
+        <h3>{props.title}</h3>
+        <p>{props.content}</p>
+    </div>
+  );
+}
+
+function List(props) {
+  // const cards = [<Card />, <Card />];
+
+  return (
+    <section className="List">
+      <header className="List-header">
+        <h2>{props.header}</h2>
+      </header>
+      <div className="List-cards">
+        {props.cards}
       </div>
-    );
-  }
+    </section>
+  );
+}
+
+function App() {
+  return (
+    <main className='App'>
+      {/* content goes here */}
+    </main>
+  );
 }
 
 export default App;

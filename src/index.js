ea62f54f21ff05be02b6578186b0f79344e8ea23
AReach <DerAReich@gmail.com>
2019-02-07 10:02:56 -0600

    updated to use state not props

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import STORE from './store';
 import App from './App';
 import './index.css';
 
-ReactDOM.render(<App store={STORE}/>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));

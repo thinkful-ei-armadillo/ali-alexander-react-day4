ca8c6af1a5b2f84b74561b5b0a01cdbab367daea
AReach <DerAReich@gmail.com>
2019-02-07 09:52:01 -0600

    initial commit for extended Trelloyes assignment

--- src/App.js
+++ src/App.js
@@ -1,50 +1,32 @@
-/* eslint-disable no-unused-expressions */
-import React from 'react';
+import React, { Component } from 'react';
+import List from './List'
+import './App.css';
 
-function Card(props) {
-  return (
-    <div className="Card">
-        <h3>{props.title}</h3>
-        <p>{props.content}</p>
-    </div>
-  );
-}
-
-function List(props) {
-  // const cards = [<Card />, <Card />];
-  const cards = props.cards.map((card) => { return <Card key = {card.id} title={card.card.title} content={card.card.content} /> });
-  return (
-    <section className="List">
-      <header className="List-header">
-        <h2>{props.header}</h2>
-      </header>
-      <div className="List-cards">
-        {cards}
-      </div>
-    </section>
-  );
-}
-
-function App(props) {
-  
-  const grabCards = function(cardIds) {
-    return cardIds.map(id => { return { id, card: props.store.allCards[id] } })
+export default class App extends Component {
+  static defaultProps = {
+    store: {
+      lists: [],
+      allCards: {},
+    }
   };
 
-  const lists = props.store.lists.map(list => {
-    return <List key={list.id} cards={grabCards(list.cardIds)} header={list.header} />
-  });
-
-  return (
-    <main className ="App">
-      <header className="App-header">
-        <h1>Trelloyes!</h1>
-      </header>
-      <div className="App-list">
-        {lists}
-      </div>
-    </main>
-  );
+  render() {
+    const { store } = this.props
+    return (
+      <main className='App'>
+        <header className='App-header'>
+          <h1>Trelloyes!</h1>
+        </header>
+        <div className='App-list'>
+          {store.lists.map(list => (
+            <List
+              key={list.id}
+              header={list.header}
+              cards={list.cardIds.map(id => store.allCards[id])}
+            />
+          ))}
+        </div>
+      </main>
+    );
+  }
 }
-
-export default App;
